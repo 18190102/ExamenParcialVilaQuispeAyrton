9e899fca93926535b13f5f415d12f0d2d1e0fa43
Ayrton Vila <51051585+18190102@users.noreply.github.com>
2019-05-26 22:47:10 -0500

    Update and rename Problema7VilaQuispe/ src/sizeofclass.java to  Problema7VilaQuispe/ src/sizeofclass.java

--- Problema7VilaQuispe/ src/sizeofclass.java
+++ Problema7VilaQuispe/ src/sizeofclass.java
@@ -1,17 +1,23 @@
-public static void main(String[] args) {
-		int x=4, w=9;
-		int i1=0;
-		for (int i=-1;i<20;i+=3) {
-			x++;
-			for(int q=4;q<11;q++) {
-				do {
-					i=+3;
-					w=4;
-					i=x+w;
-					x=w+i;
-					i1=i;
-				}while(x<15);
+paquete  ejercicio7 ;
+
+ clase  pública ProblemaFinal {
+	public  static  void  main ( String [] args ) {
+		int i, x = 4 , w = 9 , q;
+		
+		para (i = - 1 ; i < 20 ; i + = 3 ) {
+			x ++ ;
+			para (q = 4 ; q < 11 ; q ++ ) {
+				hacer {
+					i = + 3 ;
+					w = 4 ;
+					i = x + w;
+					x = w + i;
+					
+				} mientras (x < 15 );
 			}
 		}
-		System.out.println("x:"+x+" "+"i:"+i1);
+		Sistema . a cabo . println ( " x: " + x + "  " + " i: " + i);
 	}
+
+
+}
